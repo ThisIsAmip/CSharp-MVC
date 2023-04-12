16f91b1920d714098aa2f53609d040a4e5b02d93
ThisIsAmip <99376179+ThisIsAmip@users.noreply.github.com>
2023-04-12 09:02:06 +0700

    Add Entity: ProductImage and ProductInfo

---
+++ Entity/ProductInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class ProductInfo
+    {
+        public int ProductID { get; set; }
+        public string Screen { get; set; }
+        public string OS { get; set; }
+        public string FrontCam { get; set; }
+        public string BackCam { get; set; }
+        public string Chip { get; set; }
+        public string Ram { get; set; }
+        public string Storage { get; set; }
+        public string SIM { get; set; }
+        public string Battery { get; set; }
+        
+    }
+}
