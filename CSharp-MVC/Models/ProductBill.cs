ea02fd1da91e88890c78cdcdd836ff5cb23ad504
tronghk <trongnguyen.02stu@gmail.com>
2023-04-19 20:15:39 +0700

    trong -create model using controller

---
+++ CSharp-MVC/Models/ProductBill.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharp_MVC.Models
+{
+    public class ProductBill
+    {
+        public int ProductID { get; set; }
+        public int BillID { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ CSharp-MVC/Models/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharp_MVC.Models
+{
+    public class Role
+    {
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string RoleDescription { get; set; }
+    }
+}
